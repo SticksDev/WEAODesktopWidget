6663a141c605304e54af83c38d43131a08fb0492
SticksDev <tanner@teamhydra.dev>
2023-05-12 19:41:10 -0400

    fix sha

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -6,8 +6,7 @@ $logo = (Get-Item -Path ".\assets\logo.ico").FullName
 
 # Get the customtkinter path from pip show customtkinter
 $customtkinter = (pip show customtkinter | Select-String -Pattern "Location" | Select-Object -Last 1).ToString().Split(": ")[-1]
-
-# Check if this is a release build or a dev build (GITHUB_REF is only set on dev builds)
+$githubshashort = $env:GITHUB_SHA.Substring(0, 7)
 
 if ($env:GITHUB_REF) {
     # This is a dev build
@@ -15,18 +14,18 @@ if ($env:GITHUB_REF) {
     $version += (Get-Date).ToString("yyyyMMddHHmmss")
     Write-Output "BUILD: $version - dev build"
     # Run build command
-    pyinstaller --noconfirm --onefile --windowed --icon "$logo" --add-data "$assets;assets/" --add-data "$customtkinter;customtkinter/" app.py -n "WEAO-${{ github.sha }}-nightly"
+    pyinstaller --noconfirm --onefile --windowed --icon "$logo" --add-data "$assets;assets/" --add-data "$customtkinter;customtkinter/" app.py -n "WEAO-$githubshashort-$version-nightly"
 
     # Set filename for next step
-    $env:FILENAME = "WEAO-${{ github.sha }}-nightly.exe"s
+    $env:FILENAME = "WEAO-$githubshashort-$version-nightly"
 } else {
     # This is a release build
     $version = $env:GITHUB_REF.Split("/")[-1]
     Write-Output "BUILD: $version - release build"
 
     # Run build command
-    pyinstaller --noconfirm --onefile --windowed --icon "$iconAbsPath" --add-data "$assetsAbsPath;assets/" --add-data "$customtkinterAbsPath;customtkinter/" app.py -n "WEAO-${{ github.ref }}-release"
+    pyinstaller --noconfirm --onefile --windowed --icon "$iconAbsPath" --add-data "$assetsAbsPath;assets/" --add-data "$customtkinterAbsPath;customtkinter/" app.py -n "WEAO-$githubshashort-release"
 
     # Set filename for next step
-    $env:FILENAME = "WEAO-${{ github.ref }}-release.exe"
+    $env:FILENAME = "WEAO-$githubshashort-release"
 }
